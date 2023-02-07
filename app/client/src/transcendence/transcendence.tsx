082dfdaa1f953566fd6f86befd90590ea0fb91f9
Ammah <amir.mahla@icloud.com>
2023-02-07 19:48:54 +0100

    Transcendence init webapp repository

--- app/client/src/transcendence/transcendence.tsx
+++ app/client/src/transcendence/transcendence.tsx
@@ -15,19 +15,19 @@ export const Transcendence = () => {
 					Welcome to your first own full stack project ...
 			</Typography>
 			<Typography variant="h6" align="justify" padding="50px">
-					Your local volume for client ( react == localhost:3000 ) is "./app/client/"	:
+					Yours local volumes for client ( react == localhost:3000 ) are "./app/client/src" and ./app/server/public"	:
 					<br/>
-					-	you can work on directory "./app/client/transcendence"
+					-	you can work on directory "./app/client/src/transcendence"
 					<br/>
-					-	the front-end entrypoint is "./app/client/transcendence/transcendence.tsx"
+					-	the front-end entrypoint is "./app/client/src/transcendence/transcendence.tsx"
 			</Typography>
 			<Typography variant="h6" align="justify" padding="50px">
-					Your local volume for server ( nestjs == localhost:5500 ) is "./app/server/"	:
+					Your local volume for server ( nestjs == localhost:5500 ) is "./app/server/src"	:
 					<br/>
 					-	you can work on directory "./app/server/src"
 			</Typography>
 			<Typography variant="h6" align="justify" padding="50px">
-					Your local volume for postgres database is "./app/database/"	:
+					Your local volume for postgres database is handle by docker and persitent	:
 					<br/>
 					-	The database already is connected to nestjs server ( view file "/app/server/src/app.module.ts")
 			</Typography>
